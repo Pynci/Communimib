b04c57a8f12c7f7a1dfc0016eef36d39de4d65cd
Giulia Vitale <g.vitale16@campus.unimib.it>
2024-03-28 10:13:53 +0100

    setup fragment e activity per il signin e signup

---
+++ app/src/main/java/it/unimib/communimib/ui/auth/SigninFragment.java
@@ -0,0 +1,45 @@
+package it.unimib.communimib.ui.auth;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import it.unimib.communimib.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link SigninFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class SigninFragment extends Fragment {
+
+
+    public SigninFragment() {
+        // Required empty public constructor
+    }
+
+    public static SigninFragment newInstance() {
+        SigninFragment fragment = new SigninFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_signin, container, false);
+    }
+}
