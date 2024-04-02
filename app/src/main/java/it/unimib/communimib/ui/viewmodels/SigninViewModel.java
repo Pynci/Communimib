f055727c896e9ae5db608f8f8b1fd8e63815d22c
Giulia Vitale <g.vitale16@campus.unimib.it>
2024-04-02 17:01:52 +0200

    tolte dipendenze costanti e aggiunto background signup

--- app/src/main/java/it/unimib/communimib/ui/viewmodels/SigninViewModel.java
+++ app/src/main/java/it/unimib/communimib/ui/viewmodels/SigninViewModel.java
@@ -1,8 +1,5 @@
 package it.unimib.communimib.ui.viewmodels;
 
-import static it.unimib.communimib.util.ErrorMapper.EMPTY_FIELD;
-import static it.unimib.communimib.util.ErrorMapper.INVALID_FIELD;
-import static it.unimib.communimib.util.ErrorMapper.NOT_UNIVERSITY_EMAIL;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
@@ -10,10 +7,9 @@ import androidx.lifecycle.ViewModel;
 
 import org.apache.commons.validator.routines.EmailValidator;
 
-import it.unimib.communimib.Callback;
-import it.unimib.communimib.R;
 import it.unimib.communimib.model.Result;
 import it.unimib.communimib.repository.IUserRepository;
+import it.unimib.communimib.util.ErrorMapper;
 
 public class SigninViewModel extends ViewModel {
 
@@ -28,18 +24,14 @@ public class SigninViewModel extends ViewModel {
     }
 
     public void signIn (String email, String password){
-        userRepository.signIn(email, password, result -> {
-            signInResult.postValue(result);
-        });
+        userRepository.signIn(email, password, signInResult::postValue);
     }
     public LiveData<Result> getSignInResult(){
         return signInResult;
     }
 
     public void isEmailVerified(){
-        userRepository.isEmailVerified(result -> {
-            emailVerifiedResult.postValue(result);
-        });
+        userRepository.isEmailVerified(emailVerifiedResult::postValue);
     }
     public LiveData<Result> getEmailVerifiedResult(){
         return emailVerifiedResult;
@@ -47,11 +39,11 @@ public class SigninViewModel extends ViewModel {
 
     public String checkEmail(String email){
         if(email.isEmpty())
-            return EMPTY_FIELD;
+            return ErrorMapper.EMPTY_FIELD;
         if(!EmailValidator.getInstance().isValid(email))
-            return INVALID_FIELD;
+            return ErrorMapper.INVALID_FIELD;
         if (!(email.substring(email.indexOf('@'))).equals("@campus.unimib.it") && !(email.substring(email.indexOf('@'))).equals("@unimib.it"))
-            return NOT_UNIVERSITY_EMAIL;
+            return ErrorMapper.NOT_UNIVERSITY_EMAIL;
 
         return "ok";
     }
