0e99df59c9e6f0d387c5fb9fefffe70f77a6ef90
marcoferioli02 <m.ferioli@campus.unimib.it>
2024-04-12 17:58:56 +0200

    Creato dialog fragment segnalazione

---
+++ app/src/main/java/it/unimib/communimib/ui/main/reports/NewReportFragmentDialog.java
@@ -0,0 +1,30 @@
+package it.unimib.communimib.ui.main.reports;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+import it.unimib.communimib.R;
+
+public class NewReportFragmentDialog extends DialogFragment {
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        View view = requireActivity().getLayoutInflater()
+                .inflate(R.layout.fragment_new_report_dialog, null);
+        builder.setView(view);
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+        return alertDialog;
+    }
+
+}
