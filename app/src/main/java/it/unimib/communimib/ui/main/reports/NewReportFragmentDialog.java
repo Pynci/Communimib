850593a6f94679eb5beecc8224735b42f8757d2c
marcoferioli02 <m.ferioli@campus.unimib.it>
2024-04-14 11:57:05 +0200

    Completato controllo su spinner

--- app/src/main/java/it/unimib/communimib/ui/main/reports/NewReportFragmentDialog.java
+++ app/src/main/java/it/unimib/communimib/ui/main/reports/NewReportFragmentDialog.java
@@ -5,6 +5,7 @@ import android.app.Dialog;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
@@ -17,6 +18,8 @@ import java.util.Objects;
 
 import it.unimib.communimib.R;
 import it.unimib.communimib.databinding.FragmentNewReportDialogBinding;
+import it.unimib.communimib.util.ErrorMapper;
+import it.unimib.communimib.util.Validation;
 
 public class NewReportFragmentDialog extends DialogFragment {
 
@@ -42,12 +45,34 @@ public class NewReportFragmentDialog extends DialogFragment {
         ArrayAdapter<String> adapterEdificio = getBuildingsAdapter();
         binding.buildingsSpinner.setAdapter(adapterEdificio);
         binding.buildingsSpinner.setSelection(adapterEdificio.getCount());
+        binding.buildingsSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                binding.newReportErrorSpinnerBuildings.setVisibility(View.INVISIBLE);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                //Non deve fare niente
+            }
+        });
 
         //Gestione spinner categorie
         binding.categoriesSpinner.setPrompt("Categoria");
         ArrayAdapter<String> adapterCategorie = getCategoriesAdapter();
         binding.categoriesSpinner.setAdapter(adapterCategorie);
         binding.categoriesSpinner.setSelection(adapterCategorie.getCount());
+        binding.categoriesSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                binding.newReportErrorSpinnerCategories.setVisibility(View.INVISIBLE);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                //Non deve fare niente
+            }
+        });
 
         AlertDialog alertDialog = builder.create();
         Objects.requireNonNull(alertDialog.getWindow()).setBackgroundDrawableResource(android.R.color.transparent);
@@ -58,11 +83,23 @@ public class NewReportFragmentDialog extends DialogFragment {
             String buildingSpinnerSelectedItem = binding.buildingsSpinner.getSelectedItem().toString();
             String categoriesSpinnerSelectedItem =  binding.categoriesSpinner.getSelectedItem().toString();
 
-            reportsViewModel.createReport(
-                    binding.editTextReportTitle.getText().toString(),
-                    binding.editTextReportDescription.getText().toString(),
-                    buildingSpinnerSelectedItem,
-                    categoriesSpinnerSelectedItem);
+            String resultBuildingsValidation = Validation.checkBuildingsSpinner(buildingSpinnerSelectedItem);
+            String resultCategoriesValidation = Validation.checkCategoriesSpinner(categoriesSpinnerSelectedItem);
+
+            if(!resultBuildingsValidation.equals("ok")){
+                binding.newReportErrorSpinnerBuildings.setVisibility(View.VISIBLE);
+                binding.newReportErrorSpinnerBuildings.setText(ErrorMapper.getInstance().getErrorMessage(resultCategoriesValidation));
+            }
+            if(!resultCategoriesValidation.equals("ok")){
+                binding.newReportErrorSpinnerCategories.setVisibility(View.VISIBLE);
+                binding.newReportErrorSpinnerCategories.setText(ErrorMapper.getInstance().getErrorMessage(resultCategoriesValidation));
+            }
+            if(resultBuildingsValidation.equals("ok") && resultCategoriesValidation.equals("ok"))
+                reportsViewModel.createReport(
+                        binding.editTextReportTitle.getText().toString(),
+                        binding.editTextReportDescription.getText().toString(),
+                        buildingSpinnerSelectedItem,
+                        categoriesSpinnerSelectedItem);
 
         });
 
