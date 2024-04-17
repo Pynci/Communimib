2d1bd5732a586d5900d55eb9aa01fdd7f383d28d
Giulia Vitale <g.vitale16@campus.unimib.it>
2024-04-17 16:51:18 +0200

    cancellazione report su view model

--- app/src/main/java/it/unimib/communimib/ui/main/reports/ReportsViewModel.java
+++ app/src/main/java/it/unimib/communimib/ui/main/reports/ReportsViewModel.java
@@ -6,6 +6,7 @@ import androidx.lifecycle.ViewModel;
 
 import it.unimib.communimib.Callback;
 import it.unimib.communimib.DialogCallback;
+import it.unimib.communimib.model.Report;
 import it.unimib.communimib.model.Result;
 import it.unimib.communimib.repository.IReportRepository;
 import it.unimib.communimib.repository.IUserRepository;
@@ -14,11 +15,13 @@ import it.unimib.communimib.util.Validation;
 public class ReportsViewModel extends ViewModel {
 
     private final MutableLiveData<Result> createReportResult;
+    private final MutableLiveData<Result> deleteReportResult;
     private final IReportRepository reportRepository;
     private final IUserRepository userRepository;
 
     public ReportsViewModel(IReportRepository reportRepository, IUserRepository userRepository) {
         createReportResult = new MutableLiveData<>();
+        deleteReportResult = new MutableLiveData<>();
         this.reportRepository = reportRepository;
         this.userRepository = userRepository;
     }
@@ -32,6 +35,11 @@ public class ReportsViewModel extends ViewModel {
             });
         }
     }
+
+    public void deleteReport(Report report){
+        reportRepository.deleteReport(report, deleteReportResult::postValue);
+    }
+
     public LiveData<Result> getCreateReportResult() {
         return this.createReportResult;
     }
