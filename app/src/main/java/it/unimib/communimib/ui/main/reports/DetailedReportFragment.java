c30c5c9b199cecbf7d35a1e761bc4221e4985230
marcoferioli02 <m.ferioli@campus.unimib.it>
2024-04-26 10:03:44 +0200

    Iniziata implementazione della schermata per vedere il report in formato dettagliato

--- app/src/main/java/it/unimib/communimib/ui/main/reports/DetailedReportFragment.java
+++ app/src/main/java/it/unimib/communimib/ui/main/reports/DetailedReportFragment.java
@@ -3,6 +3,8 @@ package it.unimib.communimib.ui.main.reports;
 import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
@@ -10,6 +12,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import it.unimib.communimib.databinding.FragmentDetailedReportBinding;
 import it.unimib.communimib.BottomNavigationBarListener;
 import it.unimib.communimib.R;
 import it.unimib.communimib.model.Report;
@@ -17,24 +20,9 @@ import it.unimib.communimib.util.GestTopbar;
 
 public class DetailedReportFragment extends Fragment {
 
+    private FragmentDetailedReportBinding binding;
     private BottomNavigationBarListener mListener;
     private Report report;
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof BottomNavigationBarListener) {
-            mListener = (BottomNavigationBarListener) context;
-        } else {
-            throw new RuntimeException(context.toString() + " must implement BottomNavigationBarListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
 
     public DetailedReportFragment() {
         //Empty constructor
@@ -58,13 +46,37 @@ public class DetailedReportFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_detailed_report, container, false);
+        binding = FragmentDetailedReportBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.textViewTitolo.setText(report.getTitle());
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
         showBottomNavigationBar();
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof BottomNavigationBarListener) {
+            mListener = (BottomNavigationBarListener) context;
+        } else {
+            throw new RuntimeException(context.toString() + " must implement BottomNavigationBarListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
     }
 
     private void hideBottomNavigationBar() {
