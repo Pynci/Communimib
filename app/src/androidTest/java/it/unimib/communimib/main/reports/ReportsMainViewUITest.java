85eb8e4535ffec07360be5415fe3ac536467234b
marcoferioli02 <m.ferioli@campus.unimib.it>
2024-04-29 17:09:12 +0200

    Scritti altri test

--- app/src/androidTest/java/it/unimib/communimib/main/reports/ReportsMainViewUITest.java
+++ app/src/androidTest/java/it/unimib/communimib/main/reports/ReportsMainViewUITest.java
@@ -2,8 +2,7 @@ package it.unimib.communimib.main.reports;
 
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.core.app.ApplicationProvider;
-import androidx.test.espresso.Espresso;
-import androidx.test.espresso.assertion.ViewAssertions;
+import androidx.test.espresso.action.ViewActions;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
@@ -12,9 +11,18 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.Espresso.onData;
+import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withSpinnerText;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
 
 import java.util.concurrent.CountDownLatch;
 
@@ -55,60 +63,119 @@ public class ReportsMainViewUITest {
     @Test
     public void testButtonVisibility() {
         // Controllo che il bottone menu sia visibile
-        Espresso.onView(withId(R.id.floatingActionButton_menu)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+        onView(withId(R.id.floatingActionButton_menu)).check(matches(ViewMatchers.isDisplayed()));
 
         //Clico sul bottone del menu
-        Espresso.onView(withId(R.id.floatingActionButton_menu)).perform(click());
+        onView(withId(R.id.floatingActionButton_menu)).perform(click());
 
         // Controllo che il bottone preferiti sia visibile
-        Espresso.onView(withId(R.id.floatingActionButton_favorite)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+        onView(withId(R.id.floatingActionButton_favorite)).check(matches(ViewMatchers.isDisplayed()));
 
         // Controllo che il bottone degli filti-edifici sia visibile
-        Espresso.onView(withId(R.id.floatingActionButton_favorite)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+        onView(withId(R.id.floatingActionButton_favorite)).check(matches(ViewMatchers.isDisplayed()));
 
         // Controllo che il bottone di creazione sia visibile
-        Espresso.onView(withId(R.id.floatingActionButton_favorite)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+        onView(withId(R.id.floatingActionButton_favorite)).check(matches(ViewMatchers.isDisplayed()));
     }
 
     @Test
     public void testFavoriteDialogAppear() {
 
         //Clico sul bottone del menu
-        Espresso.onView(withId(R.id.floatingActionButton_menu)).perform(click());
+        onView(withId(R.id.floatingActionButton_menu)).perform(click());
 
         //Clico sul bottone dei preferiti
-        Espresso.onView(withId(R.id.floatingActionButton_favorite)).perform(click());
+        onView(withId(R.id.floatingActionButton_favorite)).perform(click());
 
         //Controllo che il dialog sia visibile
-        Espresso.onView(withText(R.string.favorite_explain_1))
-                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+        onView(withText(R.string.favorite_explain_1))
+                .check(matches(ViewMatchers.isDisplayed()));
     }
 
     @Test
     public void testCreationDialogAppear() {
 
         //Clico sul bottone del menu
-        Espresso.onView(withId(R.id.floatingActionButton_menu)).perform(click());
+        onView(withId(R.id.floatingActionButton_menu)).perform(click());
 
         //Clico sul bottone di creazione
-        Espresso.onView(withId(R.id.floatingActionButton_add_new_report)).perform(click());
+        onView(withId(R.id.floatingActionButton_add_new_report)).perform(click());
 
         //Controllo che il dialog sia visibile
-        Espresso.onView(withText(R.string.crea_una_nuova_segnalazione))
-                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+        onView(withText(R.string.crea_una_nuova_segnalazione))
+                .check(matches(ViewMatchers.isDisplayed()));
     }
 
     @Test
     public void testBuildingFilterDialogAppear() {
 
         //Clico sul bottone del menu
-        Espresso.onView(withId(R.id.floatingActionButton_menu)).perform(click());
+        onView(withId(R.id.floatingActionButton_menu)).perform(click());
 
         //Clico sul bottone dei filtri
-        Espresso.onView(withId(R.id.floatingActionButton_filter_buildings)).perform(click());
+        onView(withId(R.id.floatingActionButton_filter_buildings)).perform(click());
 
         //Controllo che il dialog sia visibile
-        Espresso.onView(withText(R.string.filtra_per_gli_edfici_preferiti))
-                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+        onView(withText(R.string.filtra_per_gli_edfici_preferiti))
+                .check(matches(ViewMatchers.isDisplayed()));
+    }
+
+    @Test
+    public void testErrorsCreateReportDialog() {
+
+        //Clico sul bottone del menu
+        onView(withId(R.id.floatingActionButton_menu)).perform(click());
+
+        //Clico sul bottone di creazione
+        onView(withId(R.id.floatingActionButton_add_new_report)).perform(click());
+
+        //Clico sul bottone di creazione
+        onView(withId(R.id.confirm_new_report)).perform(click());
+
+        //Controllo che l'errore sul titolo sia visibile
+        onView(withId(R.id.new_report_title_error))
+                .check(matches(ViewMatchers.isDisplayed()));
+
+        //Controllo che l'errore sulla descrizione sia visibile
+        onView(withId(R.id.new_report_description_error))
+                .check(matches(ViewMatchers.isDisplayed()));
+
+        //Controllo che l'errore sugli edifici sia visibile
+        onView(withId(R.id.new_report_error_spinner_buildings))
+                .check(matches(ViewMatchers.isDisplayed()));
+
+        //Controllo che l'errore sulle categorie sia visibile
+        onView(withId(R.id.new_report_error_spinner_categories))
+                .check(matches(ViewMatchers.isDisplayed()));
+    }
+
+    @Test
+    public void checkSpinnerSelectionItem() {
+
+        //Clico sul bottone del menu
+        onView(withId(R.id.floatingActionButton_menu)).perform(click());
+
+        //Clico sul bottone di creazione
+        onView(withId(R.id.floatingActionButton_add_new_report)).perform(click());
+
+        String buildingToSelect = "U1";
+
+        // Seleziona l'elemento dallo Spinner degli edifici
+        onView(withId(R.id.buildings_spinner))
+                .perform(ViewActions.click()); // Apre lo Spinner
+
+        onData(allOf(is(instanceOf(String.class)), is(buildingToSelect))).perform(click());
+
+        onView(withId(R.id.buildings_spinner)).check(matches(withSpinnerText(containsString(buildingToSelect))));
+
+        String categoryToSelect = "Guasto";
+
+        // Seleziona l'elemento dallo Spinner degli edifici
+        onView(withId(R.id.categories_spinner))
+                .perform(ViewActions.click()); // Apre lo Spinner
+
+        onData(allOf(is(instanceOf(String.class)), is(categoryToSelect))).perform(click());
+
+        onView(withId(R.id.categories_spinner)).check(matches(withSpinnerText(containsString(categoryToSelect))));
     }
 }
