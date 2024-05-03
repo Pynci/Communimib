4c19e8a3281e7ffd48382d7b727b21fa655b7d0f
Giulia Vitale <g.vitale16@campus.unimib.it>
2024-05-03 17:59:13 +0200

    piccole aggiunte su DashboardFragment

--- app/src/main/java/it/unimib/communimib/ui/main/dashboard/DashboardFragment.java
+++ app/src/main/java/it/unimib/communimib/ui/main/dashboard/DashboardFragment.java
@@ -2,15 +2,24 @@ package it.unimib.communimib.ui.main.dashboard;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import it.unimib.communimib.R;
+import it.unimib.communimib.databinding.FragmentDashboardBinding;
+import it.unimib.communimib.ui.main.reports.detailedreport.DashboardViewModelFactory;
 
 public class DashboardFragment extends Fragment {
+
+    private DashboardViewModel dashboardViewModel;
+    FragmentDashboardBinding fragmentDashboardBinding;
+    private DashboardRecyclerViewAdapter dashboardRecyclerViewAdapter;
 
     public DashboardFragment() {
         // Required empty public constructor
@@ -23,11 +32,23 @@ public class DashboardFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        dashboardViewModel = new ViewModelProvider(this,
+                new DashboardViewModelFactory(this.getContext()))
+                .get(DashboardViewModel.class);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_dashboard, container, false);
+        fragmentDashboardBinding = FragmentDashboardBinding.inflate(inflater, container, false);
+        return fragmentDashboardBinding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
     }
 }
