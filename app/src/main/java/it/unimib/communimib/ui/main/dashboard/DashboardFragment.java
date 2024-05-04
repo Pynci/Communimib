6c15b22e30cda13f0f51a8a123639d653a0de0c4
Giulia Vitale <g.vitale16@campus.unimib.it>
2024-05-04 17:01:14 +0200

    implementata gestione recycler view su DashboardFragment

--- app/src/main/java/it/unimib/communimib/ui/main/dashboard/DashboardFragment.java
+++ app/src/main/java/it/unimib/communimib/ui/main/dashboard/DashboardFragment.java
@@ -6,14 +6,24 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.logging.ErrorManager;
+
 import it.unimib.communimib.R;
 import it.unimib.communimib.databinding.FragmentDashboardBinding;
+import it.unimib.communimib.model.Post;
+import it.unimib.communimib.model.Result;
 import it.unimib.communimib.ui.main.reports.detailedreport.DashboardViewModelFactory;
+import it.unimib.communimib.util.ErrorMapper;
 
 public class DashboardFragment extends Fragment {
 
@@ -49,6 +59,57 @@ public class DashboardFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false);
+        dashboardRecyclerViewAdapter = new DashboardRecyclerViewAdapter(
+                new DashboardRecyclerViewAdapter.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(Post post) {
 
+                    }
+            }, getContext());
+
+        fragmentDashboardBinding.fragmentDashboardRecyclerView.setLayoutManager(layoutManager);
+        fragmentDashboardBinding.fragmentDashboardRecyclerView.setAdapter(dashboardRecyclerViewAdapter);
+
+        dashboardViewModel.getPostAddedReadResult().observe(getViewLifecycleOwner(), result -> {
+            if(result.isSuccessful()){
+                Post post = ((Result.PostSuccess) result).getPost();
+                dashboardRecyclerViewAdapter.addItem(post);
+            } else {
+                Snackbar.make(requireView(),
+                        ErrorMapper.getInstance().getErrorMessage(((Result.Error) result).getMessage()),
+                        BaseTransientBottomBar.LENGTH_SHORT).show();
+            }
+        });
+
+        dashboardViewModel.getPostEditedReadResult().observe(getViewLifecycleOwner(), result -> {
+            if(result.isSuccessful()){
+                Post post = ((Result.PostSuccess) result).getPost();
+                dashboardRecyclerViewAdapter.editItem(post);
+            } else {
+                Snackbar.make(requireView(),
+                        ErrorMapper.getInstance().getErrorMessage(((Result.Error) result).getMessage()),
+                        BaseTransientBottomBar.LENGTH_SHORT).show();
+            }
+        });
+
+        dashboardViewModel.getPostRemovedReadResult().observe(getViewLifecycleOwner(), result -> {
+            if(result.isSuccessful()){
+                Post post = ((Result.PostSuccess) result).getPost();
+                dashboardRecyclerViewAdapter.removeItem(post);
+            } else {
+                Snackbar.make(requireView(),
+                        ErrorMapper.getInstance().getErrorMessage(((Result.Error) result).getMessage()),
+                        BaseTransientBottomBar.LENGTH_SHORT).show();
+            }
+        });
+
+        dashboardViewModel.getPostCancelledReadResult().observe(getViewLifecycleOwner(), result ->
+            Snackbar.make(requireView(),
+                    ErrorMapper.getInstance().getErrorMessage(((Result.Error) result).getMessage()),
+                    BaseTransientBottomBar.LENGTH_SHORT).show()
+        );
+
+        dashboardViewModel.readAllPosts();
     }
 }
