590caa6aa0bff9016c5d846dffcb33a07594f232
Giulia Vitale <g.vitale16@campus.unimib.it>
2024-05-14 16:49:05 +0200

    fake dashboard data source

--- app/src/test/java/it/unimib/communimib/datasource/dashboard/FakeDashboardRemoteDataSource.java
+++ app/src/test/java/it/unimib/communimib/datasource/dashboard/FakeDashboardRemoteDataSource.java
@@ -1,4 +1,74 @@
 package it.unimib.communimib.datasource.dashboard;
 
-public class FakeDashboardRemoteDataSource {
+import java.util.Map;
+
+import it.unimib.communimib.Callback;
+import it.unimib.communimib.datasource.post.IPostRemoteDataSource;
+import it.unimib.communimib.model.Post;
+import it.unimib.communimib.model.Result;
+import it.unimib.communimib.util.ErrorMapper;
+
+public class FakeDashboardRemoteDataSource implements IPostRemoteDataSource {
+
+    public Callback addedCallback;
+    public Callback changedCallback;
+    public Callback removedCallback;
+    public Callback cancelledCallback;
+
+    public String category;
+    public String keyword;
+    public Map<String, Post> posts;
+
+    @Override
+    public void readAllPosts(Callback addedCallback, Callback changedCallback, Callback removedCallback, Callback cancelledCallback) {
+        this.addedCallback = addedCallback;
+        this.changedCallback = changedCallback;
+        this.removedCallback = removedCallback;
+        this.cancelledCallback = cancelledCallback;
+    }
+
+    @Override
+    public void readPostsByCategory(String category, Callback addedCallback, Callback changedCallback, Callback removedCallback, Callback cancelledCallback) {
+        this.category = category;
+        this.addedCallback = addedCallback;
+        this.changedCallback = changedCallback;
+        this.removedCallback = removedCallback;
+        this.cancelledCallback = cancelledCallback;
+    }
+
+    @Override
+    public void readPostsByTitleOrDescription(String keyword, Callback addedCallback, Callback changedCallback, Callback removedCallback, Callback cancelledCallback) {
+        this.keyword = keyword;
+        this.addedCallback = addedCallback;
+        this.changedCallback = changedCallback;
+        this.removedCallback = removedCallback;
+        this.cancelledCallback = cancelledCallback;
+    }
+
+    @Override
+    public void readPostsByTitleOrDescriptionAndCategory(String keyword, String category, Callback addedCallback, Callback changedCallback, Callback removedCallback, Callback cancelledCallback) {
+        this.keyword = keyword;
+        this.category = category;
+        this.addedCallback = addedCallback;
+        this.changedCallback = changedCallback;
+        this.removedCallback = removedCallback;
+        this.cancelledCallback = cancelledCallback;
+    }
+
+    @Override
+    public void createPost(Post post, Callback callback) {
+        post.setPid("12345");
+        posts.put(post.getPid(), post);
+        callback.onComplete(new Result.Success());
+    }
+
+    @Override
+    public void deletePost(Post post, Callback callback) {
+        if(posts.containsKey(post.getPid())){
+            posts.remove(post.getPid());
+            callback.onComplete(new Result.Success());
+        } else {
+            callback.onComplete(new Result.Error(ErrorMapper.POST_DELETING_ERROR));
+        }
+    }
 }
