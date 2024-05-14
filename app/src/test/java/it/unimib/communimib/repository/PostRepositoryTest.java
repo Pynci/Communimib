6bb85d5d2d44d9c87ee70c21e1f559b8eb77dcac
Giulia Vitale <g.vitale16@campus.unimib.it>
2024-05-14 17:01:24 +0200

    inizio test post repository

--- app/src/test/java/it/unimib/communimib/repository/PostRepositoryTest.java
+++ app/src/test/java/it/unimib/communimib/repository/PostRepositoryTest.java
@@ -5,19 +5,38 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import it.unimib.communimib.Callback;
 import it.unimib.communimib.datasource.dashboard.FakeDashboardRemoteDataSource;
+import it.unimib.communimib.model.Result;
 
 public class PostRepositoryTest {
 
     private FakeDashboardRemoteDataSource dashboardRemoteDataSource;
-
+    private IPostRepository postRepository;
+    private Result result;
 
     @Before
     public void setUp() throws Exception {
-
+        dashboardRemoteDataSource = new FakeDashboardRemoteDataSource();
+        postRepository = new PostRepository(dashboardRemoteDataSource);
     }
+
     @Test
     public void readAllPosts() {
+        Callback addedCallback = result -> {};
+        Callback changedCallback = result -> {};
+        Callback removedCallback = result -> {};
+        Callback cancelledCallback = result -> {};
+        postRepository.readAllPosts(
+                addedCallback,
+                changedCallback,
+                removedCallback,
+                cancelledCallback);
+
+        assertEquals(addedCallback, dashboardRemoteDataSource.addedCallback);
+        assertEquals(changedCallback, dashboardRemoteDataSource.changedCallback);
+        assertEquals(removedCallback, dashboardRemoteDataSource.removedCallback);
+        assertEquals(cancelledCallback, dashboardRemoteDataSource.cancelledCallback);
     }
 
     @Test
