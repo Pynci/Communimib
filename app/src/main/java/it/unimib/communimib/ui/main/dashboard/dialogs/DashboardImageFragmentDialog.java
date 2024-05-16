f69a2fcebec9bd5745b9ac3f197bafabff81c0d5
Luca Pinciroli <l.pinciroli3@campus.unimib.it>
2024-05-16 12:08:58 +0200

    Commit di mezzo, non funziona niente, chi tocca muore

--- app/src/main/java/it/unimib/communimib/ui/main/dashboard/dialogs/DashboardImageFragmentDialog.java
+++ app/src/main/java/it/unimib/communimib/ui/main/dashboard/dialogs/DashboardImageFragmentDialog.java
@@ -2,6 +2,8 @@ package it.unimib.communimib.ui.main.dashboard.dialogs;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 
@@ -9,7 +11,12 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.CustomTarget;
+import com.bumptech.glide.request.transition.Transition;
+import com.denzcoskun.imageslider.constants.ActionTypes;
 import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.interfaces.TouchListener;
 import com.denzcoskun.imageslider.models.SlideModel;
 
 import java.util.ArrayList;
@@ -39,9 +46,14 @@ public class DashboardImageFragmentDialog extends DialogFragment {
 
         List<SlideModel> slideModels = new ArrayList<>();
         for (String picture : post.getPictures()) {
-            slideModels.add(new SlideModel(picture, ScaleTypes.FIT));
+            slideModels.add(new SlideModel(picture, ScaleTypes.CENTER_INSIDE));
         }
-        fragmentDashboardImageDialogBinding.imageDialogImageSlider.setImageList(slideModels, ScaleTypes.FIT);
+        fragmentDashboardImageDialogBinding.imageDialogImageSlider.setImageList(slideModels, ScaleTypes.CENTER_INSIDE);
+
+        fragmentDashboardImageDialogBinding.imageDialogImageSlider.setTouchListener((actionTypes, i) -> {
+
+        });
+
 
         AlertDialog alertDialog = builder.create();
         alertDialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
