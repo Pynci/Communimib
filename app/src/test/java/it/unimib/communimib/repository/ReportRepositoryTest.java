b8e28bc648453638a6960842290fd637c90d5433
Giulia Vitale <g.vitale16@campus.unimib.it>
2024-05-16 15:24:23 +0200

    aggiornati test repository report

--- app/src/test/java/it/unimib/communimib/repository/ReportRepositoryTest.java
+++ app/src/test/java/it/unimib/communimib/repository/ReportRepositoryTest.java
@@ -1,6 +1,11 @@
 package it.unimib.communimib.repository;
 
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -14,6 +19,7 @@ import it.unimib.communimib.Callback;
 import it.unimib.communimib.R;
 import it.unimib.communimib.datasource.report.FakeReportRemoteDataSource;
 import it.unimib.communimib.datasource.report.IReportRemoteDataSource;
+import it.unimib.communimib.datasource.report.ReportRemoteDataSource;
 import it.unimib.communimib.datasource.user.FakeUserRemoteDataSource;
 import it.unimib.communimib.datasource.user.IUserRemoteDataSource;
 import it.unimib.communimib.model.Report;
@@ -25,120 +31,159 @@ public class ReportRepositoryTest {
 
     private volatile Result result;
     private IReportRepository reportRepository;
-    private FakeReportRemoteDataSource reportRemoteDataSource;
+    private IReportRemoteDataSource reportRemoteDataSource;
     private User marco;
 
     @Before
     public void setUp() throws Exception {
-        reportRemoteDataSource = new FakeReportRemoteDataSource();
+        reportRemoteDataSource = mock(ReportRemoteDataSource.class);
         reportRepository = new ReportRepository(reportRemoteDataSource);
         marco = new User("12345", "m.ferioli@campus.unimib.it", "Marco", "Ferioli", false);
     }
 
     @Test
     public void readAllReports() {
-        Callback addedCallback = result -> {};
-        Callback changedCallback = result -> {};
-        Callback removedCallback = result -> {};
-        Callback cancelledCallback = result -> {};
-        reportRepository.readAllReports(
-                addedCallback,
-                changedCallback,
-                removedCallback,
-                cancelledCallback
-        );
+        doAnswer(invocation -> {
+            Callback addedCallback = invocation.getArgument(0);
+            addedCallback.onComplete(new Result.Success());
+            Callback changedCallback = invocation.getArgument(1);
+            changedCallback.onComplete(new Result.Success());
+            Callback removedCallback = invocation.getArgument(2);
+            removedCallback.onComplete(new Result.Success());
+            Callback cancelledCallback = invocation.getArgument(3);
+            cancelledCallback.onComplete(new Result.Error(ErrorMapper.REMOTEDB_GET_ERROR));
+            return null;
+        }).when(reportRemoteDataSource).readAllReports(any(), any(), any(), any());
 
-        assertEquals(addedCallback, reportRemoteDataSource.addedCallback);
-        assertEquals(changedCallback, reportRemoteDataSource.changedCallback);
-        assertEquals(removedCallback, reportRemoteDataSource.removedCallback);
-        assertEquals(cancelledCallback, reportRemoteDataSource.cancelledCallback);
+        reportRepository.readAllReports(
+                result ->
+                        assertTrue(result instanceof Result.Success)
+                ,
+                result ->
+                        assertTrue(result instanceof Result.Success)
+                ,
+                result ->
+                        assertTrue(result instanceof Result.Success)
+                ,
+                result -> {
+                    assertTrue(result instanceof Result.Error);
+                    assertEquals(((Result.Error) result).getMessage(), ErrorMapper.REMOTEDB_GET_ERROR);
+                });
+
+        verify(reportRemoteDataSource).readAllReports(any(), any(), any(), any());
     }
 
     @Test
     public void readReportsByBuildings() {
-        Callback addedCallback = result -> {};
-        Callback changedCallback = result -> {};
-        Callback removedCallback = result -> {};
-        Callback cancelledCallback = result -> {};
         List<String> buildings = new ArrayList<>();
         buildings.add("U1");
         buildings.add("U2");
+
+        doAnswer(invocation -> {
+            Callback addedCallback = invocation.getArgument(1);
+            addedCallback.onComplete(new Result.Success());
+            Callback changedCallback = invocation.getArgument(2);
+            changedCallback.onComplete(new Result.Success());
+            Callback removedCallback = invocation.getArgument(3);
+            removedCallback.onComplete(new Result.Success());
+            Callback cancelledCallback = invocation.getArgument(4);
+            cancelledCallback.onComplete(new Result.Error(ErrorMapper.REMOTEDB_GET_ERROR));
+            return null;
+        }).when(reportRemoteDataSource).readReportsByBuildings(eq(buildings), any(), any(), any(), any());
+
         reportRepository.readReportsByBuildings(
                 buildings,
-                addedCallback,
-                changedCallback,
-                removedCallback,
-                cancelledCallback
-        );
+                result ->
+                        assertTrue(result instanceof Result.Success)
+                ,
+                result ->
+                        assertTrue(result instanceof Result.Success)
+                ,
+                result ->
+                        assertTrue(result instanceof Result.Success)
+                ,
+                result -> {
+                    assertTrue(result instanceof Result.Error);
+                    assertEquals(((Result.Error) result).getMessage(), ErrorMapper.REMOTEDB_GET_ERROR);
+                });
 
-        assertEquals(buildings, reportRemoteDataSource.buildings);
-        assertEquals(addedCallback, reportRemoteDataSource.addedCallback);
-        assertEquals(changedCallback, reportRemoteDataSource.changedCallback);
-        assertEquals(removedCallback, reportRemoteDataSource.removedCallback);
-        assertEquals(cancelledCallback, reportRemoteDataSource.cancelledCallback);
+        verify(reportRemoteDataSource).readReportsByBuildings(eq(buildings), any(), any(), any(), any());
     }
 
     @Test
     public void readReportsByTitleAndDescription() {
-        Callback addedCallback = result -> {};
-        Callback changedCallback = result -> {};
-        Callback removedCallback = result -> {};
-        Callback cancelledCallback = result -> {};
         String keyword = "keyword";
+
+        doAnswer(invocation -> {
+            Callback addedCallback = invocation.getArgument(1);
+            addedCallback.onComplete(new Result.Success());
+            Callback changedCallback = invocation.getArgument(2);
+            changedCallback.onComplete(new Result.Success());
+            Callback removedCallback = invocation.getArgument(3);
+            removedCallback.onComplete(new Result.Success());
+            Callback cancelledCallback = invocation.getArgument(4);
+            cancelledCallback.onComplete(new Result.Error(ErrorMapper.REMOTEDB_GET_ERROR));
+            return null;
+        }).when(reportRemoteDataSource).readReportsByTitleAndDescription(eq(keyword), any(), any(), any(), any());
+
         reportRepository.readReportsByTitleAndDescription(
                 keyword,
-                addedCallback,
-                changedCallback,
-                removedCallback,
-                cancelledCallback
-        );
+                result ->
+                        assertTrue(result instanceof Result.Success)
+                ,
+                result ->
+                        assertTrue(result instanceof Result.Success)
+                ,
+                result ->
+                        assertTrue(result instanceof Result.Success)
+                ,
+                result -> {
+                    assertTrue(result instanceof Result.Error);
+                    assertEquals(((Result.Error) result).getMessage(), ErrorMapper.REMOTEDB_GET_ERROR);
+                });
 
-        assertEquals(keyword, reportRemoteDataSource.keyword);
-        assertEquals(addedCallback, reportRemoteDataSource.addedCallback);
-        assertEquals(changedCallback, reportRemoteDataSource.changedCallback);
-        assertEquals(removedCallback, reportRemoteDataSource.removedCallback);
-        assertEquals(cancelledCallback, reportRemoteDataSource.cancelledCallback);
+        verify(reportRemoteDataSource).readReportsByTitleAndDescription(eq(keyword), any(), any(), any(), any());
     }
 
     @Test
-    public void createReportSuccess() throws InterruptedException {
-        CountDownLatch countDownLatch = new CountDownLatch(1);
-        reportRepository.createReport("titolo", "descrizione", "U14", "guasto", marco, result -> {
-            this.result = result;
-            countDownLatch.countDown();
-        });
-        countDownLatch.await();
-        assertTrue(result instanceof Result.Success);
+    public void createReportSuccess(){
+        Report report = new Report("titolo", "descrizione", "U14", "guasto", marco);
+        report.setRid("12345");
+        doAnswer(invocation -> {
+            Callback callback = invocation.getArgument(1);
+            callback.onComplete(new Result.Success());
+            return null;
+        }).when(reportRemoteDataSource).createReport(eq(report), any());
+
+        reportRepository.createReport(
+                "titolo",
+                "descrizione",
+                "U14",
+                "guasto",
+                marco,
+                result ->
+                        assertTrue(result instanceof Result.Success)
+        );
+
     }
 
     @Test
     public void deleteReportSuccess() throws InterruptedException {
-        CountDownLatch countDownLatch = new CountDownLatch(1);
         Report report = new Report("titolo", "descrizione", "U14", "guasto", marco);
         report.setRid("54321");
-        reportRemoteDataSource.reports.put(report.getRid(), report);
+        doAnswer(invocation -> {
+            Callback callback = invocation.getArgument(1);
+            callback.onComplete(new Result.Success());
+            return null;
+        }).when(reportRemoteDataSource).deleteReport(eq(report), any());
 
-        reportRepository.deleteReport(report, result -> {
-            this.result = result;
-            countDownLatch.countDown();
-        });
-        countDownLatch.await();
-        assertTrue(result instanceof Result.Success);
+        reportRepository.deleteReport(
+                report,
+                result ->
+                        assertTrue(result instanceof Result.Success)
+        );
+
+        verify(reportRemoteDataSource).deleteReport(eq(report), any());
     }
 
-    @Test
-    public void deleteReportFailure() throws InterruptedException {
-        CountDownLatch countDownLatch = new CountDownLatch(1);
-        Report report = new Report("titolo", "descrizione", "U14", "guasto", marco);
-        report.setRid("54321");
-        reportRemoteDataSource.reports.clear();
-
-        reportRepository.deleteReport(report, result -> {
-            this.result = result;
-            countDownLatch.countDown();
-        });
-        countDownLatch.await();
-        assertTrue(result instanceof Result.Error);
-        assertEquals(ErrorMapper.REPORT_DELETING_ERROR, ((Result.Error) result).getMessage());
-    }
 }
