62b41830474de674cb736b3cc681d274864a4c9c
Luca Pinciroli <l.pinciroli3@campus.unimib.it>
2024-05-17 17:26:03 +0200

    Implementato DetailedPostViewModel

---
+++ app/src/main/java/it/unimib/communimib/ui/main/dashboard/detailedpostwithcomments/DetailedPostViewModelFactory.java
@@ -0,0 +1,27 @@
+package it.unimib.communimib.ui.main.dashboard.detailedpostwithcomments;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+
+import it.unimib.communimib.ui.main.dashboard.DashboardViewModel;
+import it.unimib.communimib.util.ServiceLocator;
+
+public class DetailedPostViewModelFactory implements ViewModelProvider.Factory {
+    private Context context;
+
+    public DetailedPostViewModelFactory(Context context) {
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+        return (T) new DetailedPostViewModel(
+                ServiceLocator.getInstance().getPostRepository(),
+                ServiceLocator.getInstance().getUserRepository(context)
+        );
+    }
+}
