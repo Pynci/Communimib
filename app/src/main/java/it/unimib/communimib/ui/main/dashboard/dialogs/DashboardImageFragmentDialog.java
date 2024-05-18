4f229a4e4e10db526aa2250d32f0d99014415f25
Luca Pinciroli <l.pinciroli3@campus.unimib.it>
2024-05-18 16:46:44 +0200

    Aggiunto SnapHelper

--- app/src/main/java/it/unimib/communimib/ui/main/dashboard/dialogs/DashboardImageFragmentDialog.java
+++ app/src/main/java/it/unimib/communimib/ui/main/dashboard/dialogs/DashboardImageFragmentDialog.java
@@ -8,7 +8,9 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.LinearSnapHelper;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.SnapHelper;
 
 import it.unimib.communimib.databinding.FragmentDashboardImageDialogBinding;
 import it.unimib.communimib.model.Post;
@@ -33,6 +35,9 @@ public class DashboardImageFragmentDialog extends DialogFragment {
         DashboardImageRecyclerViewAdapter dashboardImageRecyclerViewAdapter = new DashboardImageRecyclerViewAdapter(post.getPictures(), getContext());
         fragmentDashboardImageDialogBinding.dashboardImageDialogRecyclerView.setLayoutManager(layoutManager);
         fragmentDashboardImageDialogBinding.dashboardImageDialogRecyclerView.setAdapter(dashboardImageRecyclerViewAdapter);
+
+        SnapHelper snapHelper = new LinearSnapHelper();
+        snapHelper.attachToRecyclerView(fragmentDashboardImageDialogBinding.dashboardImageDialogRecyclerView);
 
         AlertDialog alertDialog = builder.create();
         alertDialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
