1dbe11396e2a07b9c04fbce14f0e9289454c02a6
Giulia Vitale <g.vitale16@campus.unimib.it>
2024-05-21 18:52:55 +0200

    implementati test commenti su repository

--- app/src/test/java/it/unimib/communimib/repository/PostRepositoryTest.java
+++ app/src/test/java/it/unimib/communimib/repository/PostRepositoryTest.java
@@ -17,6 +17,7 @@ import it.unimib.communimib.datasource.post.CommentRemoteDataSource;
 import it.unimib.communimib.datasource.post.ICommentRemoteDataSource;
 import it.unimib.communimib.datasource.post.IPostRemoteDataSource;
 import it.unimib.communimib.datasource.post.PostRemoteDataSource;
+import it.unimib.communimib.model.Comment;
 import it.unimib.communimib.model.Post;
 import it.unimib.communimib.model.Result;
 import it.unimib.communimib.model.User;
@@ -53,13 +54,13 @@ public class PostRepositoryTest {
 
         postRepository.readAllPosts(
                 result ->
-                    assertTrue(result instanceof Result.Success)
+                        assertTrue(result instanceof Result.Success)
                 ,
                 result ->
-                    assertTrue(result instanceof Result.Success)
+                        assertTrue(result instanceof Result.Success)
                 ,
                 result ->
-                    assertTrue(result instanceof Result.Success)
+                        assertTrue(result instanceof Result.Success)
                 ,
                 result -> {
                     assertTrue(result instanceof Result.Error);
@@ -88,13 +89,13 @@ public class PostRepositoryTest {
         postRepository.readPostsByCategory(
                 category,
                 result ->
-                    assertTrue(result instanceof Result.Success)
+                        assertTrue(result instanceof Result.Success)
                 ,
                 result ->
-                    assertTrue(result instanceof Result.Success)
+                        assertTrue(result instanceof Result.Success)
                 ,
                 result ->
-                    assertTrue(result instanceof Result.Success)
+                        assertTrue(result instanceof Result.Success)
                 ,
                 result -> {
                     assertTrue(result instanceof Result.Error);
@@ -123,13 +124,13 @@ public class PostRepositoryTest {
         postRepository.readPostsByTitleOrDescription(
                 keyword,
                 result ->
-                    assertTrue(result instanceof Result.Success)
+                        assertTrue(result instanceof Result.Success)
                 ,
                 result ->
-                    assertTrue(result instanceof Result.Success)
+                        assertTrue(result instanceof Result.Success)
                 ,
                 result ->
-                    assertTrue(result instanceof Result.Success)
+                        assertTrue(result instanceof Result.Success)
                 ,
                 result -> {
                     assertTrue(result instanceof Result.Error);
@@ -160,13 +161,13 @@ public class PostRepositoryTest {
                 keyword,
                 category,
                 result ->
-                    assertTrue(result instanceof Result.Success)
+                        assertTrue(result instanceof Result.Success)
                 ,
                 result ->
-                    assertTrue(result instanceof Result.Success)
+                        assertTrue(result instanceof Result.Success)
                 ,
                 result ->
-                    assertTrue(result instanceof Result.Success)
+                        assertTrue(result instanceof Result.Success)
                 ,
                 result -> {
                     assertTrue(result instanceof Result.Error);
@@ -195,8 +196,8 @@ public class PostRepositoryTest {
                 "https://link",
                 new ArrayList<>(),
                 result ->
-                    assertTrue(result instanceof Result.Success)
-                );
+                        assertTrue(result instanceof Result.Success)
+        );
 
         verify(postRemoteDataSource).createPost(eq(post), any());
     }
@@ -215,10 +216,62 @@ public class PostRepositoryTest {
         postRepository.deletePost(
                 post,
                 result ->
-                    assertTrue(result instanceof Result.Success)
-                );
+                        assertTrue(result instanceof Result.Success)
+        );
 
         verify(postRemoteDataSource).deletePost(eq(post), any());
+    }
+
+    @Test
+    public void readCommentsByPid() {
+        Post post = new Post();
+        post.setPid("11111");
+
+        doAnswer(invocation -> {
+            Callback addedCallback = invocation.getArgument(1);
+            addedCallback.onComplete(new Result.Success());
+            Callback changedCallback = invocation.getArgument(2);
+            changedCallback.onComplete(new Result.Success());
+            Callback removedCallback = invocation.getArgument(3);
+            removedCallback.onComplete(new Result.Success());
+            Callback cancelledCallback = invocation.getArgument(4);
+            cancelledCallback.onComplete(new Result.Error(ErrorMapper.REMOTEDB_GET_ERROR));
+            return null;
+        }).when(commentRemoteDataSource).readCommentsByPid(eq(post.getPid()), any(), any(), any(), any());
+
+        postRepository.readCommentsByPid(
+                post.getPid(),
+                result ->
+                        assertTrue(result instanceof Result.Success),
+                result ->
+                        assertTrue(result instanceof Result.Success),
+                result ->
+                        assertTrue(result instanceof Result.Success),
+                result -> {
+                    assertTrue(result instanceof Result.Error);
+                    assertEquals(((Result.Error) result).getMessage(), ErrorMapper.REMOTEDB_GET_ERROR);
+                });
+    }
+
+    @Test
+    public void createComment() {
+        Comment comment = new Comment(user, "commento", System.currentTimeMillis());
+        Post post = new Post();
+        post.setPid("11111");
+
+        doAnswer(invocation -> {
+            Callback callback = invocation.getArgument(2);
+            callback.onComplete(new Result.Success());
+            return null;
+        }).when(commentRemoteDataSource).createComment(eq(post.getPid()), eq(comment), any());
+
+        postRepository.createComment(
+                post.getPid(),
+                user,
+                "commento",
+                result ->
+                        assertTrue(result instanceof Result.Success)
+        );
     }
 
 }
