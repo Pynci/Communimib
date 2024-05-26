75efe0b9eb1241dea70e42222e30a2af5d4cbbe7
Luca Pinciroli <l.pinciroli3@campus.unimib.it>
2024-05-26 15:34:11 +0200

    Parzialmente riscritto PostRemoteDataSource

--- app/src/main/java/it/unimib/communimib/datasource/post/PostRemoteDataSource.java
+++ app/src/main/java/it/unimib/communimib/datasource/post/PostRemoteDataSource.java
@@ -10,124 +10,59 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.CountDownLatch;
 
 import it.unimib.communimib.Callback;
 import it.unimib.communimib.model.Post;
 import it.unimib.communimib.model.Result;
-import it.unimib.communimib.model.User;
 import it.unimib.communimib.util.Constants;
 import it.unimib.communimib.util.ErrorMapper;
 
 public class PostRemoteDataSource implements IPostRemoteDataSource{
 
     private final DatabaseReference databaseReference;
+    private final List<ChildEventListener> currentListeners;
+    private final List<DatabaseReference> currentReferences;
     private int picturesCounter;
     private int numberOfPictures;
 
     public PostRemoteDataSource() {
         FirebaseDatabase.getInstance().setPersistenceEnabled(true);
         this.databaseReference = FirebaseDatabase.getInstance(Constants.DATABASE).getReference();
+        currentListeners = new ArrayList<>();
+        currentReferences = new ArrayList<>();
     }
 
     @Override
     public void readAllPosts(Callback addedCallback, Callback changedCallback, Callback removedCallback, Callback cancelledCallback) {
-
-        databaseReference
-                .child(Constants.POST_PATH)
-                .limitToFirst(10)
-                .addChildEventListener(new ChildEventListener() {
-            @Override
-            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-                Post post = snapshot.getValue(Post.class);
-                post.setPid(snapshot.getKey());
-
-                addedCallback.onComplete(new Result.PostSuccess(post));
-            }
-
-            @Override
-            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-                Post post = snapshot.getValue(Post.class);
-                post.setPid(snapshot.getKey());
-
-                changedCallback.onComplete(new Result.PostSuccess(post));
-            }
-
-            @Override
-            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
-                Post post = snapshot.getValue(Post.class);
-                post.setPid(snapshot.getKey());
-
-                removedCallback.onComplete(new Result.PostSuccess(post));
-            }
-
-            @Override
-            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-                //per ora niente
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-                cancelledCallback.onComplete(new Result.Error(ErrorMapper.REMOTEDB_GET_ERROR));
-            }
-        });
+        removeAllQueryListeners();
+        addQueryListener(null, null, addedCallback, changedCallback, removedCallback, cancelledCallback);
     }
 
     @Override
-    public void readPostsByCategory(String queryParameter, Callback addedCallback, Callback changedCallback, Callback removedCallback, Callback cancelledCallback) {
+    public void readPostsByCategory(String category, Callback addedCallback, Callback changedCallback, Callback removedCallback, Callback cancelledCallback) {
+        removeAllQueryListeners();
+        addQueryListener("category", category, addedCallback, changedCallback, removedCallback, cancelledCallback);
+    }
 
-        databaseReference
-                .child(Constants.POST_PATH)
-                .orderByChild("category")
-                .equalTo(queryParameter)
-                .addChildEventListener(new ChildEventListener() {
-                    @Override
-                    public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-                        Post post = snapshot.getValue(Post.class);
-                        post.setPid(snapshot.getKey());
-
-                        addedCallback.onComplete(new Result.PostSuccess(post));
-                    }
-
-                    @Override
-                    public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-                        Post post = snapshot.getValue(Post.class);
-                        post.setPid(snapshot.getKey());
-
-                        changedCallback.onComplete(new Result.PostSuccess(post));
-                    }
-
-                    @Override
-                    public void onChildRemoved(@NonNull DataSnapshot snapshot) {
-                        Post post = snapshot.getValue(Post.class);
-                        post.setPid(snapshot.getKey());
-
-                        removedCallback.onComplete(new Result.PostSuccess(post));
-                    }
-
-                    @Override
-                    public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-                        //per ora niente
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError error) {
-                        cancelledCallback.onComplete(new Result.Error(ErrorMapper.REMOTEDB_GET_ERROR));
-                    }
-                });
+    @Override
+    public void readPostsByUid(String uid, Callback addedCallback, Callback changedCallback, Callback removedCallback, Callback cancelledCallback) {
+        removeAllQueryListeners();
+        addQueryListener("author/uid", uid, addedCallback, changedCallback, removedCallback, cancelledCallback);
     }
 
     @Override
     public void readPostsByTitleOrDescription(String keyword, Callback addedCallback, Callback changedCallback, Callback removedCallback, Callback cancelledCallback) {
-        databaseReference
-                .child(Constants.POST_PATH)
-                .addChildEventListener(new ChildEventListener() {
+        removeAllQueryListeners();
+        Query query = databaseReference.child(Constants.POST_PATH);
+        currentReferences.add(query.getRef());
+
+        currentListeners.add(new ChildEventListener() {
             @Override
             public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
                 Post post = snapshot.getValue(Post.class);
@@ -163,7 +98,7 @@ public class PostRemoteDataSource implements IPostRemoteDataSource{
 
             @Override
             public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-                // per ora niente, nel caso aggiungere una callback
+                //per ora niente
             }
 
             @Override
@@ -171,103 +106,54 @@ public class PostRemoteDataSource implements IPostRemoteDataSource{
                 cancelledCallback.onComplete(new Result.Error(ErrorMapper.REMOTEDB_GET_ERROR));
             }
         });
+
+        query.addChildEventListener(currentListeners.get(0));
     }
 
-    public void readPostsByTitleOrDescriptionAndCategory(String keyword,
-                                                         String category,
-                                                         Callback addedCallback,
-                                                         Callback changedCallback,
-                                                         Callback removedCallback,
-                                                         Callback cancelledCallback){
+    public void readPostsByTitleOrDescriptionAndCategory(String keyword, String category,
+                                                         Callback addedCallback, Callback changedCallback,
+                                                         Callback removedCallback, Callback cancelledCallback){
+        removeAllQueryListeners();
+        Query query = databaseReference.child(Constants.POST_PATH).orderByChild("category").equalTo(category);
+        currentReferences.add(query.getRef());
 
-        databaseReference
-                .child(Constants.POST_PATH)
-                .orderByChild("category")
-                .equalTo(category)
-                .addChildEventListener(new ChildEventListener() {
-                    @Override
-                    public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-                        Post post = snapshot.getValue(Post.class);
-                        post.setPid(snapshot.getKey());
-                        String title = post.getTitle().toLowerCase();
-                        String description = post.getDescription().toLowerCase();
-                        if(title.contains(keyword) || description.contains(keyword)){
-                            addedCallback.onComplete(new Result.PostSuccess(post));
-                        }
-                    }
-
-                    @Override
-                    public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-                        Post post = snapshot.getValue(Post.class);
-                        post.setPid(snapshot.getKey());
-                        String title = post.getTitle().toLowerCase();
-                        String description = post.getDescription().toLowerCase();
-                        if(title.contains(keyword) || description.contains(keyword)){
-                            changedCallback.onComplete(new Result.PostSuccess(post));
-                        }
-                    }
-
-                    @Override
-                    public void onChildRemoved(@NonNull DataSnapshot snapshot) {
-                        Post post = snapshot.getValue(Post.class);
-                        post.setPid(snapshot.getKey());
-                        String title = post.getTitle().toLowerCase();
-                        String description = post.getDescription().toLowerCase();
-                        if(title.contains(keyword) || description.contains(keyword)){
-                            removedCallback.onComplete(new Result.PostSuccess(post));
-                        }
-                    }
-
-                    @Override
-                    public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-                        //per ora niente
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError error) {
-                        cancelledCallback.onComplete(new Result.Error(ErrorMapper.REMOTEDB_GET_ERROR));
-                    }
-                });
-    }
-
-    @Override
-    public void readPostsByUid(String uid,
-                               Callback addedCallback,
-                               Callback changedCallback,
-                               Callback removedCallback,
-                               Callback cancelledCallback) {
-        databaseReference
-                .child(Constants.POST_PATH)
-                .orderByChild("author/uid")
-                .equalTo(uid)
-                .addChildEventListener(new ChildEventListener() {
+        currentListeners.add(new ChildEventListener() {
             @Override
             public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
                 Post post = snapshot.getValue(Post.class);
                 post.setPid(snapshot.getKey());
-
-                addedCallback.onComplete(new Result.PostSuccess(post));
+                String title = post.getTitle().toLowerCase();
+                String description = post.getDescription().toLowerCase();
+                if(title.contains(keyword) || description.contains(keyword)){
+                    addedCallback.onComplete(new Result.PostSuccess(post));
+                }
             }
 
             @Override
             public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
                 Post post = snapshot.getValue(Post.class);
                 post.setPid(snapshot.getKey());
-
-                changedCallback.onComplete(new Result.PostSuccess(post));
+                String title = post.getTitle().toLowerCase();
+                String description = post.getDescription().toLowerCase();
+                if(title.contains(keyword) || description.contains(keyword)){
+                    changedCallback.onComplete(new Result.PostSuccess(post));
+                }
             }
 
             @Override
             public void onChildRemoved(@NonNull DataSnapshot snapshot) {
                 Post post = snapshot.getValue(Post.class);
                 post.setPid(snapshot.getKey());
-
-                removedCallback.onComplete(new Result.PostSuccess(post));
+                String title = post.getTitle().toLowerCase();
+                String description = post.getDescription().toLowerCase();
+                if(title.contains(keyword) || description.contains(keyword)){
+                    removedCallback.onComplete(new Result.PostSuccess(post));
+                }
             }
 
             @Override
             public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-
+                //per ora niente
             }
 
             @Override
@@ -275,6 +161,8 @@ public class PostRemoteDataSource implements IPostRemoteDataSource{
                 cancelledCallback.onComplete(new Result.Error(ErrorMapper.REMOTEDB_GET_ERROR));
             }
         });
+
+        query.addChildEventListener(currentListeners.get(0));
     }
 
 
@@ -300,7 +188,6 @@ public class PostRemoteDataSource implements IPostRemoteDataSource{
     private void createPostWithPictures(Post post, int i, List<String> downloadUris, Callback callback) {
         StorageReference postStorageReference = FirebaseStorage.getInstance().getReference()
                 .child(Constants.STORAGE_POSTPICS).child(post.getPid()).child(String.valueOf(i));
-
 
         //carico l'immagine corrente sul Firebase Storage
         postStorageReference
@@ -383,5 +270,72 @@ public class PostRemoteDataSource implements IPostRemoteDataSource{
                         callback.onComplete(new Result.Error(ErrorMapper.POST_DELETING_ERROR));
                     }
                 });
+    }
+
+    private void removeAllQueryListeners(){
+        if(!currentListeners.isEmpty() && !currentReferences.isEmpty()){
+            for (int i = 0; i < currentListeners.size(); i++) {
+                currentReferences.get(i).removeEventListener(currentListeners.get(i));
+            }
+            currentListeners.clear();
+            currentReferences.clear();
+        }
+    }
+
+    private void addQueryListener(String path, String queryParameter,
+                                  Callback addedCallback,
+                                  Callback changedCallback,
+                                  Callback removedCallback,
+                                  Callback cancelledCallback) {
+        Query query;
+
+        if((queryParameter == null || queryParameter.isEmpty()) && (path == null || path.isEmpty())){
+             query = databaseReference
+                    .child(Constants.POST_PATH)
+                    .limitToFirst(30);
+        }
+        else{
+            query = databaseReference
+                    .child(Constants.POST_PATH)
+                    .orderByChild(path)
+                    .equalTo(queryParameter);
+        }
+
+        currentReferences.add(query.getRef());
+        ChildEventListener listener = new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                Post post = snapshot.getValue(Post.class);
+                post.setPid(snapshot.getKey());
+                addedCallback.onComplete(new Result.PostSuccess(post));
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                Post post = snapshot.getValue(Post.class);
+                post.setPid(snapshot.getKey());
+                changedCallback.onComplete(new Result.PostSuccess(post));
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+                Post post = snapshot.getValue(Post.class);
+                post.setPid(snapshot.getKey());
+                removedCallback.onComplete(new Result.PostSuccess(post));
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                // per ora niente, nel caso aggiungere una callback
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                cancelledCallback.onComplete(new Result.Error(ErrorMapper.REMOTEDB_GET_ERROR));
+            }
+        };
+
+        currentListeners.add(listener);
+        query.addChildEventListener(listener);
     }
 }
